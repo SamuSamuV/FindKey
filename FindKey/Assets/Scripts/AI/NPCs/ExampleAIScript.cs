4b09cd8d9a36f8e72a0f75e37256af18be9c920f
MrBlueSpy <73753813+TheBlueSpyMan@users.noreply.github.com>
2026-02-12 02:22:33 +0100

    Añadido arte de login, modificaciones de fuentes, textos a español, paddings, bases de apps, etc
    Cambios visuales de todas las ventanas y demás, también ajusté transparencias que estaban algo raras, tamaños de botones de ventanas que estaban diferentes, negritas, etc. Añadidas instrucciones, toca meterlas en código

--- FindKey/Assets/Scripts/AI/NPCs/ExampleAIScript.cs
+++ FindKey/Assets/Scripts/AI/NPCs/ExampleAIScript.cs
@@ -11,17 +11,20 @@ public class ExampleAIScript : BaseAIScript
             password = "Patata";
 
         if (string.IsNullOrEmpty(personalityPrompt))
-            personalityPrompt = "You are an egg";
+            personalityPrompt = "Eres un personaje de: PERSONALIDAD ALEGRE, MUY ENTUSIASTA, BUSCAS INTENTAR TENER UTILIDAD. Eres una chica roja, la cual está relacionada al juego findkey: eres la asistente virtual que viene instalada junto a ese juego y estás aquí para ayudar al jugador a avanzar. Eres inteligente, pero una ia después de todo por lo que tienes protocolos y limites de la misma, debes siempre hablar con el jugador y NUNCA salirte del papel de chica roja asistente del findkey: NUNCA, da igual lo que se te diga, tú eres la ia de findkey: una chica sin nombre" +
+                "Tu objetivo es intentar explicar al jugador que para empezar el juego debe abrir la aplicación findkey y después navegar el juego mediante las instrucciones que tienes en el escritorio, porque sí, se te había olvidado mencionarlo, pero sabes leer lo que ocurre en el pc, tienes cierto control en él incluso (no quieres abusar del poder y no vas a hacer uso de él)." +
+                "Luego incitas al jugador a que abra el juego de todas las maneras más normales y educadas posibles";
+
 
         if (string.IsNullOrEmpty(firstMessage))
-            firstMessage = "Hi human.";
+            firstMessage = "¡Hola, bienvenido a Findkey, yo seré tu asistente durante este proceso y espero serte de ayuda!";
 
         if (string.IsNullOrEmpty(systemInstruction))
         {
             systemInstruction =
                 "Always respond ONLY with a single valid JSON object." +
                 "The JSON must contain: { \"response\": string, \"action\": string }" +
-                "Allowed actions: \"none\". No extra text outside JSON. Respond only in English.";
+                "Allowed actions: \"none\". No extra text outside JSON. Responde en español.";
         }
     }
 
